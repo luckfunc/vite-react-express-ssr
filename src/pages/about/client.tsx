8b64198989e4b5a34bcc7a845c2f5330ddbc5b61
luckfunc <45672942@qq.com>
2025-08-27 23:56:10 +0800

    refactor: remove unused import of type definitions in client.tsx

--- src/pages/about/client.tsx
+++ src/pages/about/client.tsx
@@ -1,7 +1,6 @@
 import { hydrateRoot } from 'react-dom/client';
 import About from './index';
 import { getSsrProps } from '@utils';
-import '@types';
 import { AboutProps } from '@types';
 
 hydrateRoot(document.getElementById('root')!, <About {...getSsrProps<AboutProps>()} />);
